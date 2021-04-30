c868b3aa693361924ea8c39332783094dc3a9a6c
Stanoque <stanoquedzhumpsh@gmail.com>
2021-04-30 15:36:40 +0300

    Sticky header added.

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,12 @@
-function toggleMobileSearch() {
-	$header = $('#header');
+var SCROLL_EPSILON = 10;
 
-	
-	$input = $('#header-search-input');
-	$logo = $('#header-logo');
-	$navbar_toggler = $('#header-navbar-toggler');
-	$search_icon_mobile = $('#header-search-icon-mobile');
+var $header = $('#header');
+var $input = $('#header-search-input');
+var $logo = $('#header-logo');
+var $navbar_toggler = $('#header-navbar-toggler');
+var $search_icon_mobile = $('#header-search-icon-mobile');
+
+function toggleMobileSearch() {
 
 	if( $header.hasClass('search-toggled-mock') )
 	{
@@ -41,3 +42,31 @@ function toggleMobileNavbar() {
 function focusSearch() {
 	$('#navbar-search-input').focus();
 }
+
+var lastScrollTop = 0;
+
+window.addEventListener("scroll", function(){
+
+	var st = window.pageYOffset || document.documentElement.scrollTop;
+	 if (Math.abs(st - lastScrollTop) > SCROLL_EPSILON || !st) {
+   		if (st > lastScrollTop || !st){
+				$header.addClass("header-sticky-beyond-screen");
+
+				setTimeout(function() {
+					$header.removeClass("header-sticky");
+					$header.removeClass("header-sticky-on-screen");
+					$header.removeClass("header-sticky-beyond-screen");
+				}, 250)
+   		} else {
+				if (!$header.attr("class").split(/\s+/).includes("header-sticky")) {
+					$header.addClass("header-sticky");
+		
+					setTimeout(function() {
+						$header.addClass("header-sticky-on-screen");
+					}, 0)
+			}
+   	}
+	}
+
+   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false);
